8f5a19c043d1efe5041a90f8d17f252597e06058
Giotreglia <giovanni.treglia@gmail.com>
2023-03-08 13:13:18 +0100

    Inizializzazione

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// OUR TEAM
+
+/*
+
+Creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: 
+nome, ruolo e foto.
+
+*/
+
+// Creo lista membri del team
+
+
+
+
+
