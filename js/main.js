db95e2db2098a1e1ca126575de93b11f755a132d
Giotreglia <giovanni.treglia@gmail.com>
2023-03-08 13:20:57 +0100

    Milestone 0 - creato array di oggetti

--- js/main.js
+++ js/main.js
@@ -10,7 +10,16 @@ nome, ruolo e foto.
 
 // Creo lista membri del team
 
+let teamList = [
+    { nome: "Wayne Barnett", posizione: "Founder & CEO", foto: "wayne-barnett-founder-ceo.jpg" },
+    { nome: "Angela Caroll", posizione: "Chief Editor", foto: "angela-caroll-chief-editor.jpg" },
+    { nome: "Walter Gordon", posizione: "Office Manager", foto: "walter-gordon-office-manager.jpg" },
+    { nome: "Angela Lopez", posizione: "Social Media Manager", foto: "angela-lopez-social-media-manager.jpg" },
+    { nome: "Scott Estrada", posizione: "Developer", foto: "scott-estrada-developer.jpg" },
+    { nome: "Barbara Ramos", posizione: "Graphic Designer", foto: "barbara-ramos-graphic-designer.jpg" },
+];
 
+console.log(teamList);
 
 
 
